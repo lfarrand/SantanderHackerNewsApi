77f19d89b44714a5f40057a770ebc4978c9334a3
Lee Farrand <lee@leefarrand.com>
2026-09-07 09:24:06 +0100

    Resolved PR comments

--- src/HackerNews.BestStories.Blazor/Program.cs
+++ src/HackerNews.BestStories.Blazor/Program.cs
@@ -33,7 +33,7 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
 }
 
 app.UseStaticFiles();
